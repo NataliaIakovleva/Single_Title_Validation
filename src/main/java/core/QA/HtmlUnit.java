cef418c7ae324662d610e2e6d3ab174e15f61861
Natalia Iakovleva <natalia.iakovleva@hotmail.com>
2015-01-24 16:46:01 -0800

    Added argument variables for url and expected title

--- src/main/java/core/QA/HtmlUnit.java
+++ src/main/java/core/QA/HtmlUnit.java
@@ -5,13 +5,22 @@ import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 
 public class HtmlUnit {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] test_data) {
+		
+		if (test_data.length == 0) 
+		{
+			System.err.println("No arguments!");
+			System.exit(1);
+		}
 
 		WebDriver driver = new HtmlUnitDriver();
 		String test_case_id = "01.01";
-		String url = "http://www.yandex.ru/";
-		String title_expected = "Яндекс";
+//		String url = "http://www.yandex.ru/";
+//		String title_expected = "Яндекс";
+		String param[] = test_data[0].split("\\|");
+		
+		String url = param[0];
+		String title_expected = param[1];
 		
 		driver.get(url);
 		if (title_expected.equals(driver.getTitle()))
